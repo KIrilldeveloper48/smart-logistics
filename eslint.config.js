51b86b71dca1806b0f81a6fab720bb5f2177c540
Kirill Popov <58586831+KIrilldeveloper48@users.noreply.github.com>
2026-07-30 21:27:31 +0300

    chore: add code quality tooling

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['coverage', 'dist', 'node_modules'],
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat['recommended-latest'],
+      reactRefresh.configs.vite,
+      prettierConfig,
+    ],
+  },
+);
